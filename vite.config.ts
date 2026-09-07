549efc0be42a76fc745753c57b4b7c5442dc8fc7
rahmat342 <rahmatrahmat342@gmail.com>
2026-09-07 12:45:09 +0700

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: "/Pawon-e-simbah/",
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
