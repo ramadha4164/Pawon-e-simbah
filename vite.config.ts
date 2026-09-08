3c8883926e927118b5787e5fbca621ebaff29f51
rahmat342 <rahmatrahmat342@gmail.com>
2026-09-08 21:19:35 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from "node:url";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/Pawon-e-simba/",
+  base: "/Pawon-e-simbah/",
 
   resolve: {
     alias: {
